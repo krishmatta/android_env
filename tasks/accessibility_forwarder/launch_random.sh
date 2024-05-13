2d9600188ec2efcba14044f9f605a9825bab6161
Krish Matta <self@krishxmatta.dev>
2024-05-12 23:34:03 -0700

    Fix broken launcher

--- tasks/accessibility_forwarder/launch_random.sh
+++ tasks/accessibility_forwarder/launch_random.sh
@@ -1,7 +1,8 @@
 python ../../examples/run_random_agent.py \
---avd_name='my_avd' \
---android_avd_home='/Users/krishxmatta/.android/avd' \
---android_sdk_root='/Users/krishxmatta/Library/Android/sdk' \
---emulator_path='/Users/krishxmatta/Library/Android/sdk/platform-tools/adb' \
---num_steps=1000 \
---task_path=accessibility_forwarder_clock_set_timer.textproto
+       --avd_name='my_avd' \
+       --android_avd_home=/Users/krishxmatta/.android/avd \
+       --android_sdk_root=/Users/krishxmatta/Library/Android/sdk \
+       --emulator_path=/Users/krishxmatta/Library/Android/sdk/emulator/emulator \
+       --adb_path=/Users/krishxmatta/Library/Android/sdk/platform-tools/adb \
+       --num_steps=1000 \
+       --task_path=accessibility_forwarder_clock_set_timer.textproto
